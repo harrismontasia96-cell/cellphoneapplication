bfb8d5c7407faaef9c97807f6481af3d242f8f52
montasia harris <mharris@my.yearupunited.com>
2025-09-30 16:05:34 -0400

    cellphone exercise wk2wkbk2

---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        class cellphone {
+            private serialNumber("1000000 - 9999999") {
+
+            }
+
+            private model("i Phone x");
+
+
+
+    }
+}
