990759694dc293fd3385150be0e8c45dbdc48bec
montasia harris <mharris@my.yearupunited.com>
2025-10-01 11:10:41 -0400

    cellphone exercise wk2wkbk2

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,15 +1,67 @@
 package com.pluralsight;
 
 public class CellPhoneApplication {
+    private int serialNumber;
+    private String model, carrier, phoneNumber, owner;
+
+
+    public CellPhoneApplication() {
+        this.serialNumber = 0;
+        this.model = "iphone" ;
+        this.carrier = "boost" ;
+        this.phoneNumber = "412808745" ;
+        this.owner = "Tasia" ;
+    }
+
+
     public static void main(String[] args) {
-        class cellphone {
-            private serialNumber("1000000 - 9999999") {
 
-            }
+    }
 
-            private model("i Phone x");
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel() {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier() {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String  phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String  owner) {
+        this.owner = owner;
+
+
 
 
 
     }
 }
+
+
+
